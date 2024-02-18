47cb021a112360cc100e4e8880325e3156550c8d
mozzammelrobi <mozzammelfb@gmail.com>
2024-02-18 21:26:32 +0600

    click backgroud color and success section after complete registration is finished and copone code discount is also complete

--- js/script.js
+++ js/script.js
@@ -28,14 +28,60 @@ for(const btn of btns){
         btn.style.backgroundColor = "green";
         btn.classList.add('text-white')
        
-
+        
         appendChildAndSet('seat-name-container',p)
         appendChildAndSet('class-name',p2 )
         appendChildAndSet('price',p3)    
         
+        
+        setInnerText('grand-total', totalPriceSum)
         setInnerText('total-price', totalPriceSum)
         setInnerText('seat-count', count)
         
+        btn.disabled = true;
+        btn.style.color = 'white'
+
         
    })
+}
+
+
+
+function nextBtn(){
+    const cmpBtn = document.getElementById('success-section')
+    cmpBtn.classList.remove('hidden')
+
+    document.getElementById('header').classList.add('hidden')
+    document.getElementById('best-offer').classList.add('hidden')
+    document.getElementById('paribahan').classList.add('hidden')
+}
+
+
+
+
+document.getElementById('copone').addEventListener('keyup', function(e){
+    const text = e.target.value;
+    const coponeBtn = document.getElementById('copone-btn')
+    if(text === 'NEW15' || text === 'Couple 20'){
+        coponeBtn.removeAttribute('disabled')
+    }
+    else{
+        coponeBtn.setAttribute('disabled',true)
+    }
+})
+
+
+
+function discount(){
+    
+    const grandTotalButton = document.getElementById('grand-total').innerText
+    const convertedGrandTotal = parseInt(grandTotalButton) 
+        
+   const discount1 =  convertedGrandTotal * 15 /100;
+   const finalGrandTotal = convertedGrandTotal - discount1
+   setInnerText('grand-total', finalGrandTotal)
+  
+    
+    const coponeContainer = document.getElementById('copone-container')
+    coponeContainer.classList.add('hidden')
 }
--- js/utility.js
+++ js/utility.js
@@ -9,3 +9,19 @@ function appendChildAndSet(id, value){
     const set = document.getElementById(id);
     set.appendChild(value)
 }
+
+
+
+
+const nameInput = document.getElementById('name-input')
+const numberInput = document.getElementById('phone-number-input')
+
+const enableButton = document.getElementById('next-btn')
+
+nameInput.addEventListener('input', function(){
+    if (nameInput.value.trim() !== '' || numberInput.value.trim() !== '') {
+        enableButton.removeAttribute('disabled');
+      } else {
+        enableButton.setAttribute('disabled',true);
+      }
+})
